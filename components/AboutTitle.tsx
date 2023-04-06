fed54d683b9682a975eb3487e4a1c03cea1d6f19
hori <fourbasic19@gmail.com>
2023-04-07 08:30:28 +0900

    【jsx】aboutページ作成　キャッシュ確認

---
+++ components/AboutTitle.tsx
@@ -0,0 +1,13 @@
+const AboutTitle = () => {
+  return (
+          <div>
+            <h3>アバウトコンポーネントのタイトル</h3>
+            <a href="#">こちらはコンポーネントのりんくです</a>
+            <p>dfasdfsaffdsfsdfsds111111111111111111</p>
+            <hr />
+          </div>
+
+  );
+};
+
+export default AboutTitle;
---
+++ pages/about.tsx
@@ -0,0 +1,13 @@
+import AboutTitle from "../components/AboutTitle";
+
+export default function About() {
+
+  return (
+    <>
+      <main className="container mx-auto py-4">
+        <AboutTitle />
+        <p>会社概要とか商品歴とかfdadfsdfd</p>
+      </main>
+    </>
+  )
+}
