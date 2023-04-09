f5d8cb65ce2a5785c927f58e621cda5ddd785cba
hori <fourbasic19@gmail.com>
2023-04-09 18:10:39 +0900

    【prettier】prettier　ローカル設定

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from 'react';
 
 const Post = () => {
   return (
     <div>
       <h2>ページ詳細内容を下に表示していく</h2>
     </div>
-  )
-}
+  );
+};
 
 export default Post;
