9181f88115200a5d4384848f0955faef1177a311
hori <fourbasic19@gmail.com>
2023-04-09 20:14:23 +0900

    【tailwind】詳細ページのcss粗で作成

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -2,8 +2,15 @@ import React from 'react';
 
 const Post = () => {
   return (
-    <div>
-      <h2>ページ詳細内容を下に表示していく</h2>
+    <div className='container mx-auto my-2 sm:my-4'>
+      <h2 className='text-2xl font-medium'>ページ詳細内容を下に表示していく</h2>
+      <i className='border-b-2 w-1/3 mt-1 border-sky-900 block'></i>
+      <span className='text-gray-500'>aaaa</span>
+      <br />
+      <p className='text-white bg-sky-900 rounded-xl font-medium mt-2 px-2 inline-block'>
+        Typescript
+      </p>
+      <div className='mt-10 font-medium'>dafsadsdgfsdfsdfsdfsasfdaf</div>
     </div>
   );
 };
