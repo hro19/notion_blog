6575210e3992e6cbf62caf2d3bf5facf3853eeda
hori <fourbasic19@gmail.com>
2023-04-10 06:33:13 +0900

    【tailwind】gridとgrid-template-columnsを練習

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,55 +1,106 @@
-import { useState } from "react";
+import { useState } from 'react';
 import Image from 'next/image';
-import AboutTitle from "../components/AboutTitle";
+import AboutTitle from '../components/AboutTitle';
 
 export default function About() {
-
   type User = {
     name: string;
     age: number;
-  }
-  const [users, setUsers] = useState<User[]>([{ name: "yamashi", age: 34 },{ name: "adachi", age: 46 }]);
-
+  };
+  const [users, setUsers] = useState<User[]>([
+    { name: 'yamashi', age: 34 },
+    { name: 'adachi', age: 46 },
+  ]);
 
   return (
     <>
       <main>
-        <div className="container mx-auto py-4">
-        <AboutTitle />
-        <div className="container mx-auto sm:flex sm:justify-between">
-          <div className="bg-red-800 sm:w-[calc(50%_-_10px)] px-4">aaaaa</div>
-          <div className="bg-orange-500 sm:w-[calc(50%_-_10px)] px-4">bbb<br />bbb<br />bbb</div>
-        </div>
-
-          <div className="relative grid place-items-center h-60 bg-blue-200 mb-[40px]">
-            <i className="relative w-[180px] h-[120px]">
-          <Image
-            className="object-contain"
-            src="https://placehold.jp/3d4070/ffffff/150x90.png"
-            alt="プレイス画像"
-            fill
-          />
-          </i>
-        </div>
-        
-        <div className="container mx-auto">
-          <div className="flex flex-wrap sm:ml-[1.3%] ml-[1.5%]">
-              <div className="w-[calc(50%_-_2%)] mr-[2.0%] mb-[30px] bg-yellow-600 sm:w-[calc(33%_-_1%)] sm:mr-[1.3%]"><p className="text-4xl">ユーティリティとしてのサイズ</p></div>
-              <div className="w-[calc(50%_-_2%)] mr-[2.0%] mb-[30px] bg-yellow-600 sm:w-[calc(33%_-_1%)] sm:mr-[1.3%]"><p className="text-2xl">ユーティリティとしてのサイズ</p></div>
-              <div className="w-[calc(50%_-_2%)] mr-[2.0%] mb-[30px] bg-yellow-600 sm:w-[calc(33%_-_1%)] sm:mr-[1.3%]"><p className="text-[14px]">フォントサイズをそのまま指定</p></div>
-              <div className="w-[calc(50%_-_2%)] mr-[2.0%] mb-[30px] bg-yellow-600 sm:w-[calc(33%_-_1%)] sm:mr-[1.3%]"><p className="text-[1.5rem]">16の何倍になります</p></div>
-              <div className="w-[calc(50%_-_2%)] mr-[2.0%] mb-[30px] bg-yellow-600 sm:w-[calc(33%_-_1%)] sm:mr-[1.3%]"><p className="font-bold">太くなります</p></div>
-              <div className="w-[calc(50%_-_2%)] mr-[2.0%] mb-[30px] bg-yellow-600 sm:w-[calc(33%_-_1%)] sm:mr-[1.3%]"><p className="text-center">真ん中に</p></div>
-              <div className="w-[calc(50%_-_2%)] mr-[2.0%] mb-[30px] bg-yellow-600 sm:w-[calc(33%_-_1%)] sm:mr-[1.3%]"><p className="underline">下線を引く</p></div>
+        <div className='container mx-auto py-4'>
+          <AboutTitle />
+          <div className='container mx-auto sm:flex sm:justify-between'>
+            <div className='bg-red-800 sm:w-[calc(50%_-_10px)] px-4'>aaaaa</div>
+            <div className='bg-orange-500 sm:w-[calc(50%_-_10px)] px-4'>
+              bbb
+              <br />
+              bbb
+              <br />
+              bbb
+            </div>
           </div>
-        </div>
-            {users.map((user :User, index:number) => (
-              <div key={index}>
-                {`名前は、${ user.name }と申し、${ user.age }歳でございます。`}
+
+          <div className='relative grid place-items-center h-60 bg-blue-200 mb-[40px]'>
+            <i className='relative w-[180px] h-[120px]'>
+              <Image
+                className='object-contain'
+                src='https://placehold.jp/3d4070/ffffff/150x90.png'
+                alt='プレイス画像'
+                fill
+              />
+            </i>
+          </div>
+
+          <div className='container mx-auto'>
+            <div className='flex flex-wrap sm:ml-[1.3%] ml-[1.5%]'>
+              <div className='w-[calc(50%_-_2%)] mr-[2.0%] mb-[30px] bg-yellow-600 sm:w-[calc(33%_-_1%)] sm:mr-[1.3%]'>
+                <p className='text-4xl'>ユーティリティとしてのサイズ</p>
               </div>
-            ))}
+              <div className='w-[calc(50%_-_2%)] mr-[2.0%] mb-[30px] bg-yellow-600 sm:w-[calc(33%_-_1%)] sm:mr-[1.3%]'>
+                <p className='text-2xl'>ユーティリティとしてのサイズ</p>
+              </div>
+              <div className='w-[calc(50%_-_2%)] mr-[2.0%] mb-[30px] bg-yellow-600 sm:w-[calc(33%_-_1%)] sm:mr-[1.3%]'>
+                <p className='text-[14px]'>フォントサイズをそのまま指定</p>
+              </div>
+              <div className='w-[calc(50%_-_2%)] mr-[2.0%] mb-[30px] bg-yellow-600 sm:w-[calc(33%_-_1%)] sm:mr-[1.3%]'>
+                <p className='text-[1.5rem]'>16の何倍になります</p>
+              </div>
+              <div className='w-[calc(50%_-_2%)] mr-[2.0%] mb-[30px] bg-yellow-600 sm:w-[calc(33%_-_1%)] sm:mr-[1.3%]'>
+                <p className='font-bold'>太くなります</p>
+              </div>
+              <div className='w-[calc(50%_-_2%)] mr-[2.0%] mb-[30px] bg-yellow-600 sm:w-[calc(33%_-_1%)] sm:mr-[1.3%]'>
+                <p className='text-center'>真ん中に</p>
+              </div>
+              <div className='w-[calc(50%_-_2%)] mr-[2.0%] mb-[30px] bg-yellow-600 sm:w-[calc(33%_-_1%)] sm:mr-[1.3%]'>
+                <p className='underline'>下線を引く</p>
+              </div>
+            </div>
+          </div>
+          {users.map((user: User, index: number) => (
+            <div key={index}>
+              {`名前は、${user.name}と申し、${user.age}歳でございます。`}
+            </div>
+          ))}
+        </div>
+        <div className='container mx-auto'>
+          <div
+            className='grid gap-4 sm:grid-cols-3 mx-2 sm:mx-2'
+            style={{
+              gridTemplateColumns: 'repeat(auto-fit, minmax(16rem, 1fr))',
+            }}
+          >
+            <div className=' bg-yellow-800'>
+              <p className='text-4xl'>ユーティリティとしてのサイズ</p>
+            </div>
+            <div className=' bg-yellow-800'>
+              <p className='text-2xl'>ユーティリティとしてのサイズ</p>
+            </div>
+            <div className=' bg-yellow-800'>
+              <p className='text-[14px]'>フォントサイズをそのまま指定</p>
+            </div>
+            <div className=' bg-yellow-800'>
+              <p className='text-[1.5rem]'>16の何倍になります</p>
+            </div>
+            <div className=' bg-yellow-800'>
+              <p className='font-bold'>太くなります</p>
+            </div>
+            <div className=' bg-yellow-800'>
+              <p className='text-center'>真ん中に</p>
+            </div>
+            <div className=' bg-yellow-800'>
+              <p className='underline'>下線を引く</p>
+            </div>
+          </div>
         </div>
       </main>
     </>
-  )
+  );
 }
