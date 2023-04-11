2cbc6f4badba32ed5c2b703ea1ea30d58523e3f4
hori <fourbasic19@gmail.com>
2023-04-12 08:47:30 +0900

    【Next】リンクの中にリンクを作成

--- components/TopPost.tsx
+++ components/TopPost.tsx
@@ -14,35 +14,39 @@ type Props = {
 const TopPost = (props: Props) => {
   const { id, title, date, tags, slug, thumbnail } = props;
   return (
-    <Link
-      href={`/posts/${slug}`}
+    <div
+      key={id}
       className='rounded-lg overflow-hidden shadow-2xl hover:shadow-lg hover:translate-y-1 transition-all duration-300 text-sky-600'
     >
-      <div key={id}>
-        <div className='relative w-full h-[120px] md:h-[192px]'>
+      <div className='relative w-full h-[120px] md:h-[192px]'>
+        <Link href={`/posts/${slug}`}>
           <Image
             className='w-full h-48 object-cover'
             src={thumbnail}
             alt={title}
             fill
           />
-        </div>
-        <div className='p-4'>
+        </Link>
+      </div>
+      <div className='p-4'>
+        <Link href={`/posts/${slug}`} passHref>
           <h2 className='text-lg font-semibold'>{title}</h2>
           <p className='text-gray-600 text-sm mb-2'>{date}</p>
           <div className='flex flex-wrap mb-2'>
             {tags.map((tag: any, index: any) => (
-              <span
-                key={index}
-                className='bg-gray-200 text-gray-700 text-xs font-semibold px-2 py-1 rounded-full mr-2 mb-2'
-              >
-                {tag}
-              </span>
+              <Link href={`/tags/${tag}`} passHref>
+                <span
+                  key={index}
+                  className='bg-gray-200 text-gray-700 text-xs font-semibold px-2 py-1 rounded-full mr-2 mb-2'
+                >
+                  {tag}
+                </span>
+              </Link>
             ))}
           </div>
-        </div>
+        </Link>
       </div>
-    </Link>
+    </div>
   );
 };
 
