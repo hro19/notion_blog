1db62a757c37302872eb3dc326d07ec2f0cbd743
hori <fourbasic19@gmail.com>
2023-04-11 13:04:53 +0900

    【jsx】ハンバーガーメニューをtoggle

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,19 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <>
       <header>
         <nav className='bg-gray-200 border-gray-200'>
           <div className='max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4'>
             <Link href='/' className='flex items-center'>
-              <span className='self-center text-2xl font-semibold whitespace-nowrap text-amber-400 md:text-emerald-600 text-center'>
+              <span className='self-center text-2xl font-semibold whitespace-nowrap text-amber-600 md:text-emerald-600 text-center'>
                 技術記事🐰
               </span>
             </Link>
             <button
-              data-collapse-toggle='navbar-default'
+              onClick={toggleMenu}
               type='button'
               className='inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600'
               aria-controls='navbar-default'
@@ -35,7 +41,9 @@ const Navbar = () => {
               </svg>
             </button>
             <div
-              className='hidden w-full md:block md:w-auto'
+              className={`${
+                isMenuOpen ? 'block' : 'hidden'
+              } w-full md:block md:w-auto`}
               id='navbar-default'
             >
               <ul className='font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700'>
