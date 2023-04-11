5d68d0903675dfe86340f77fc7e85e4d6b6b840a
hori <fourbasic19@gmail.com>
2023-04-11 13:50:26 +0900

    【jsx】syntax-highlighterを使ってcode部を装飾する

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { GetStaticProps } from 'next';
 import { getAllPosts, getSinglePost } from '@/lib/notionAPI';
 import { ReactMarkdown } from 'react-markdown/lib/react-markdown';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { a11yDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 
 //https:zenn.dev/yumiyoshi/scraps/f6220f26263103
 //getStaticPathsはサーバーサイドでビルド時にレンダリングする必要のあるパスのリストを生成する
@@ -34,7 +36,7 @@ export const getStaticProps: GetStaticProps = async ({ params }: any) => {
 
 const Post = ({ post }: any) => {
   return (
-    <div className='container mx-auto my-2 sm:my-4'>
+    <div className='content-wrap container mx-auto my-2 sm:my-4'>
       <h2 className='text-2xl font-medium'>{post.metadata.title}</h2>
       <i className='border-b-2 w-1/3 mt-1 border-sky-900'></i>
       <span className='text-gray-500'>投稿日：{post.metadata.date}</span>
@@ -47,8 +49,29 @@ const Post = ({ post }: any) => {
           {tag}
         </p>
       ))}
-      <div className='content'>
-        <ReactMarkdown>{post.markdown}</ReactMarkdown>
+      <div className='content mt-8'>
+        <ReactMarkdown
+          components={{
+            code({ node, inline, className, children, ...props }) {
+              const match = /language-(\w+)/.exec(className || '');
+              return !inline && match ? (
+                <SyntaxHighlighter
+                  {...props}
+                  children={String(children).replace(/\n$/, '')}
+                  style={a11yDark}
+                  language={match[1]}
+                  PreTag='div'
+                />
+              ) : (
+                <code {...props} className={className}>
+                  {children}
+                </code>
+              );
+            },
+          }}
+        >
+          {post.markdown}
+        </ReactMarkdown>
       </div>
     </div>
   );
