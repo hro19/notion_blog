ab0bc08d39fc61b1367a539fd1fb646d25f74a4f
hori <fourbasic19@gmail.com>
2023-04-11 17:24:37 +0900

    【jsx】tag記事一覧のたたき台

---
+++ pages/tags/[tag].tsx
@@ -0,0 +1,47 @@
+import { GetStaticProps } from 'next';
+
+import { getAllPosts } from '../../lib/notionAPI';
+import TopPost from '../../components/TopPost';
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  let params = [{ params: { tag: 'tailwind' } }, { params: { tag: 'css' } }];
+
+  return {
+    paths: params,
+    fallback: 'blocking',
+  };
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  const allPosts = await getAllPosts();
+
+  return {
+    props: {
+      allPosts,
+    },
+    revalidate: 10,
+  };
+};
+
+export default function PostsByTag({ allPosts }: any) {
+  // console.log(allPosts);
+  return (
+    <>
+      <main className='container mx-auto py-4'>
+        <div className='px-4 sm:px-6 lg:px-8 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 lg:gap-6'>
+          {allPosts.map((card: any) => (
+            <TopPost
+              id={card.id}
+              title={card.title}
+              date={card.date}
+              tags={card.tags}
+              slug={card.slug}
+              thumbnail={card.thumbnail}
+              key={card.id}
+            />
+          ))}
+        </div>
+      </main>
+    </>
+  );
+}
