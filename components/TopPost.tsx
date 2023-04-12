95d286245ea7b85a129394b4f5e863a51c0fa666
hori <fourbasic19@gmail.com>
2023-04-12 12:38:27 +0900

    【バグフィックス】indexの位置を変更

--- components/TopPost.tsx
+++ components/TopPost.tsx
@@ -34,11 +34,8 @@ const TopPost = (props: Props) => {
           <p className='text-gray-600 text-sm mb-2'>{date}</p>
           <div className='flex flex-wrap mb-2'>
             {tags.map((tag: any, index: any) => (
-              <Link href={`/tags/${tag}`} passHref>
-                <span
-                  key={index}
-                  className='bg-gray-200 text-gray-700 text-xs font-semibold px-2 py-1 rounded-full mr-2 mb-2'
-                >
+              <Link href={`/tags/${tag}`} key={index} passHref>
+                <span className='bg-gray-200 text-gray-700 text-xs font-semibold px-2 py-1 rounded-full mr-2 mb-2'>
                   {tag}
                 </span>
               </Link>
