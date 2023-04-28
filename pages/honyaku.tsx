ed1747f28ed7e67014aefdd21ce9ba71b689c65e
hori <fourbasic19@gmail.com>
2023-04-28 18:39:06 +0900

    【SpeechSynthesisUtterance】音声読み上げボタンを設置、文字が日本語か英語で音声振り分け。

--- pages/honyaku.tsx
+++ pages/honyaku.tsx
@@ -8,30 +8,35 @@ const Honyaku = () => {
   const [inputText, setInputText] = useState(''); // inputText ステートを宣言する
   const [translatedText, setTranslatedText] = useState(''); // translatedText ステートを宣言する
   const [apiLimit, setApiLimit] = useState(500000); // apiLimit ステートを宣言する
+  const [isJapanese, setIsJapanese] = useState(false); // isJapanese ステートを宣言する
 
+  //インプットした文字をinputTextにセットする
   const handleInputChange = (event) => {
     setInputText(event.target.value);
   };
 
+  //翻訳ハンドラー
   const handleTranslate = () => {
     let deeplInput = inputText;
-    let isJapanese = false;
+    let isJap = false;
     let sourceLang = '';
 
     for (var i = 0; i < deeplInput.length; i++) {
       //言語判別
       if (deeplInput.charCodeAt(i) >= 256) {
-        isJapanese = true;
+        isJap = true;
         break;
       }
     }
 
-    switch (isJapanese) {
+    switch (isJap) {
       case true:
         sourceLang = '&source_lang=JA&target_lang=EN';
+        setIsJapanese(true);
         break;
       case false:
         sourceLang = '&source_lang=EN&target_lang=JA';
+        setIsJapanese(false);
         break;
       default:
         alert('言語の判別に失敗しました');
@@ -59,6 +64,7 @@ const Honyaku = () => {
       });
   };
 
+  //一月のご利用文字数を表示
   useEffect(() => {
     const getApiLimit = () => {
       const content = encodeURI('auth_key=' + API_KEY);
@@ -83,27 +89,52 @@ const Honyaku = () => {
     getApiLimit();
   }, [translatedText]);
 
+  //音声読み上げハンドラー
+  const handleSpeak = () => {
+    const utterance = new SpeechSynthesisUtterance(translatedText);
+    if (isJapanese) {
+      utterance.lang = 'en-US';
+    } else {
+      utterance.lang = 'ja-JP';
+    }
+    speechSynthesis.speak(utterance);
+  };
+
   return (
     <div className='p-4'>
-      <h1 className='text-2xl font-bold mb-4'>翻訳ページ</h1>
-      {!isNaN(apiLimit) && `${apiLimit}文字`}（500000文字まで）
-      <textarea
-        id='deepl-input'
-        value={inputText}
-        onChange={handleInputChange}
-        className='p-2 rounded-md border border-gray-300 shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 w-full h-48'
-      />
-      <button
-        onClick={handleTranslate}
-        className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-md mt-4'
-      >
-        Translate
-      </button>
-      <div
-        id='deepl-output'
-        className='p-2 rounded-md border border-gray-300 shadow-sm mt-4 h-24'
-      >
-        {translatedText}
+      <h1 className='text-2xl font-bold mb-4 text-center'>翻訳ページ</h1>
+      <p className='mb-3'>
+        {!isNaN(apiLimit) && `${apiLimit}文字`}（500000文字まで）
+      </p>
+      <div className='grid gap-4 sm:grid-cols-2'>
+        <div>
+          <textarea
+            id='deepl-input'
+            value={inputText}
+            onChange={handleInputChange}
+            className='p-2 rounded-md border border-gray-300 shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 w-full h-36'
+          />
+          <button
+            onClick={handleTranslate}
+            className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-md mt-4'
+          >
+            Translate
+          </button>
+        </div>
+        <div>
+          <div
+            id='deepl-output'
+            className='p-2 rounded-md border border-gray-300 shadow-sm h-36'
+          >
+            {translatedText}
+          </div>
+          <button
+            onClick={handleSpeak}
+            className='bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-md mt-4'
+          >
+            Speak
+          </button>
+        </div>
       </div>
     </div>
   );
