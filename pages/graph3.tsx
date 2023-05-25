b722bf36e8c6a32fe4d68911cb78fbd631f0cb86
hori <fourbasic19@gmail.com>
2023-05-25 18:40:54 +0900

    【graphql】コンテンツ情報を出力しやすくするために専用の配列に格納

--- pages/graph3.tsx
+++ pages/graph3.tsx
@@ -10,9 +10,15 @@ interface Post {
   content: string;
 }
 
+interface ImageData {
+  img: string;
+  thumbnail: string;
+}
+
 export default function Graphql() {
   const [post, setPost] = useState<Post | null>(null);
   const [loading, setLoading] = useState(true);
+  const [dls, setDls] = useState<ImageData[]>([]);
 
   useEffect(() => {
     const client = new ApolloClient({
@@ -36,6 +42,7 @@ export default function Graphql() {
           `,
         });
         setPost(data.gallery);
+        extractDls(data.gallery.content); // extractDlsを呼び出す
       } catch (error) {
         console.error(error);
       } finally {
@@ -46,20 +53,46 @@ export default function Graphql() {
     fetchPost();
   }, []);
 
+  const extractDls = (content: string) => {
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(content, 'text/html');
+    const dlElements = doc.getElementsByTagName('dl');
+    const extractedDls: ImageData[] = [];
+
+    for (let i = 0; i < dlElements.length; i++) {
+      const dl = dlElements[i];
+      const anchorElement = dl.querySelector('a');
+      const img = anchorElement?.getAttribute('href') || '';
+      const thumbnail =
+        anchorElement?.querySelector('img')?.getAttribute('src') || '';
+
+      extractedDls.push({ img, thumbnail });
+    }
+
+    setDls(extractedDls);
+  };
+
   if (loading) return <p>Loading...</p>;
 
   return (
     <div>
       <div>
         <h2 className='text-2xl'>
-          【{post.galleryId}】{post.title}
+          【{post?.galleryId}】{post?.title}
         </h2>
-        <p>{post.date}</p>
-        <Link href={`/blog/${post.galleryId}`} className='underline font-bold'>
+        <p>{post?.date}</p>
+        <Link href={`/blog/${post?.galleryId}`} className='underline font-bold'>
           Read More
         </Link>
         <hr />
-        <div className='contents'>{post.content}</div>
+
+        {/* dlsをマップして表示 */}
+        {dls.map((dl, index) => (
+          <div key={index}>
+            <img src={dl.thumbnail} alt={post?.title} />
+            <img src={dl.img} alt={post?.title} />
+          </div>
+        ))}
       </div>
     </div>
   );
