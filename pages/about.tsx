d50929980da7f3756f21c657c4c88d3c5b3b7a8c
hori <fourbasic19@gmail.com>
2023-05-26 18:56:42 +0900

    【戻し】練習更新

--- pages/about.tsx
+++ pages/about.tsx
@@ -4,98 +4,19 @@ import AboutTitle from '../components/AboutTitle';
 import AboutMenu from '../components/AboutMenu';
 
 export default function About() {
-  type User = {
-    name: string;
-    age: number;
-  };
-  const [users, setUsers] = useState<User[]>([
-    { name: 'yamashi', age: 34 },
-    { name: 'adachi', age: 46 },
-  ]);
-
   return (
     <>
       <main>
         <div className='container mx-auto py-4'>
           <AboutTitle />
           <ul className='flex justify-center gap-2 text-base'>
-            <AboutMenu slug={'flex'} menutext={'flex構造'} />
-            <AboutMenu slug={'xy_center'} menutext={'上下中央寄せ'} />
             <AboutMenu slug={'grid'} menutext={'grid構造'} />
+            <AboutMenu slug={'xy_center'} menutext={'上下中央寄せ'} />
+            <AboutMenu slug={'flex'} menutext={'flex構造'} />
             <AboutMenu slug={'member'} menutext={'リスト一覧'} />
           </ul>
         </div>
-        <div className='container mx-auto py-4'>
-          <h2
-            id='flex'
-            className='text-center text-2xl mb-3'
-            style={{ scrollMarginTop: '20px' }}
-          >
-            【flex構造】
-          </h2>
-          <div className='container mx-auto sm:flex sm:justify-between pb-2'>
-            <div className='bg-red-600 sm:w-[calc(50%_-_10px)] px-4'>
-              bg-red-600
-            </div>
-            <div className='bg-lime-500 sm:w-[calc(50%_-_10px)] px-4'>
-              bg-lime-500
-              <br />
-              bbb
-              <br />
-              bbb
-            </div>
-          </div>
-
-          <div className='container mx-auto'>
-            <div className='flex flex-wrap sm:ml-[1.3%] ml-[1.5%]'>
-              <div className='w-[calc(50%_-_2%)] mr-[2.0%] mb-[30px] bg-slate-300 sm:w-[calc(33%_-_1%)] sm:mr-[1.3%]'>
-                <p className='text-orange-400 text-3xl'>text-orange-400</p>
-              </div>
-              <div className='w-[calc(50%_-_2%)] mr-[2.0%] mb-[30px] bg-slate-300 sm:w-[calc(33%_-_1%)] sm:mr-[1.3%]'>
-                <p className='text-green-500 text-3xl'>text-green-500</p>
-              </div>
-              <div className='w-[calc(50%_-_2%)] mr-[2.0%] mb-[30px] bg-slate-300 sm:w-[calc(33%_-_1%)] sm:mr-[1.3%]'>
-                <p className='text-purple-700 text-3xl'>text-purple-700</p>
-              </div>
-              <div className='w-[calc(50%_-_2%)] mr-[2.0%] mb-[30px] bg-slate-300 sm:w-[calc(33%_-_1%)] sm:mr-[1.3%]'>
-                <p className='font-bold'>太くなります</p>
-              </div>
-              <div className='w-[calc(50%_-_2%)] mr-[2.0%] mb-[30px] bg-slate-300 sm:w-[calc(33%_-_1%)] sm:mr-[1.3%]'>
-                <p className='text-center'>真ん中に</p>
-              </div>
-              <div className='w-[calc(50%_-_2%)] mr-[2.0%] mb-[30px] bg-slate-300 sm:w-[calc(33%_-_1%)] sm:mr-[1.3%]'>
-                <p className='underline'>下線を引く</p>
-              </div>
-              <div className='w-[calc(50%_-_2%)] mr-[2.0%] mb-[30px] bg-slate-300 sm:w-[calc(33%_-_1%)] sm:mr-[1.3%]'>
-                <p className='underline'>下線を引く</p>
-              </div>
-            </div>
-          </div>
-
-          <h2
-            id='xy_center'
-            className='text-center text-2xl mb-3'
-            style={{ scrollMarginTop: '20px' }}
-          >
-            【上下中央寄せ】
-          </h2>
-          <div className='relative grid place-items-center h-60 bg-green-600 mb-[40px]'>
-            <i className='relative w-[180px] h-[120px]'>
-              <Image
-                className='object-contain'
-                src='https://placehold.jp/3d4070/ffffff/150x90.png'
-                alt='プレイス画像'
-                fill
-              />
-            </i>
-          </div>
-
-          {users.map((user: User, index: number) => (
-            <div key={index}>
-              {`名前は、${user.name}と申し、${user.age}歳でございます。`}
-            </div>
-          ))}
-        </div>
+        {/* ----------------------------- */}
         <div className='container mx-auto'>
           <h2
             id='grid'
@@ -138,6 +59,76 @@ export default function About() {
             </div>
           </div>
         </div>
+        {/* ----------------------------- */}
+        <div className='container mx-auto py-4'>
+          <h2
+            id='xy_center'
+            className='text-center text-2xl mb-3'
+            style={{ scrollMarginTop: '20px' }}
+          >
+            【上下中央寄せ】
+          </h2>
+          <div className='relative grid place-items-center h-60 bg-green-600 mb-[40px]'>
+            <i className='relative w-[180px] h-[120px]'>
+              <Image
+                className='object-contain'
+                src='https://placehold.jp/3d4070/ffffff/150x90.png'
+                alt='プレイス画像'
+                fill
+              />
+            </i>
+          </div>
+        </div>
+        {/* ----------------------------- */}
+        <div className='container mx-auto py-4'>
+          <h2
+            id='flex'
+            className='text-center text-2xl mb-3'
+            style={{ scrollMarginTop: '20px' }}
+          >
+            【flex構造】
+          </h2>
+          <div className='container mx-auto sm:flex sm:justify-between pb-2'>
+            <div className='bg-red-600 sm:w-[calc(50%_-_10px)] px-4'>
+              bg-red-600
+            </div>
+            <div className='bg-lime-500 sm:w-[calc(50%_-_10px)] px-4'>
+              bg-lime-500
+              <br />
+              bbb
+              <br />
+              bbb
+            </div>
+          </div>
+          <div className='flex flex-wrap sm:ml-[1.3%] ml-[1.5%]'>
+            <div className='w-[calc(50%_-_2%)] mr-[2.0%] mb-[30px] bg-slate-300 sm:w-[calc(33%_-_1%)] sm:mr-[1.3%]'>
+              <p className='text-orange-400 text-3xl'>text-orange-400</p>
+            </div>
+            <div className='w-[calc(50%_-_2%)] mr-[2.0%] mb-[30px] bg-slate-300 sm:w-[calc(33%_-_1%)] sm:mr-[1.3%]'>
+              <p className='text-green-500 text-3xl'>text-green-500</p>
+            </div>
+            <div className='w-[calc(50%_-_2%)] mr-[2.0%] mb-[30px] bg-slate-300 sm:w-[calc(33%_-_1%)] sm:mr-[1.3%]'>
+              <p className='text-purple-700 text-3xl'>text-purple-700</p>
+            </div>
+            <div className='w-[calc(50%_-_2%)] mr-[2.0%] mb-[30px] bg-slate-300 sm:w-[calc(33%_-_1%)] sm:mr-[1.3%]'>
+              <p className='text-cyan-600 text-3xl'>text-cyan-600 text-3xl</p>
+            </div>
+            <div className='w-[calc(50%_-_2%)] mr-[2.0%] mb-[30px] bg-slate-300 sm:w-[calc(33%_-_1%)] sm:mr-[1.3%]'>
+              <p className='text-teal-400 text-base'>
+                text-teal-400 text-base'
+              </p>
+            </div>
+            <div className='w-[calc(50%_-_2%)] mr-[2.0%] mb-[30px] bg-slate-300 sm:w-[calc(33%_-_1%)] sm:mr-[1.3%]'>
+              <p className='text-violet-600 text-center'>
+                text-violet-600 text-center
+              </p>
+            </div>
+            <div className='w-[calc(50%_-_2%)] mr-[2.0%] mb-[30px] bg-slate-300 sm:w-[calc(33%_-_1%)] sm:mr-[1.3%]'>
+              <p className='text-rose-700 text-end'>text-rose-700 text-end</p>
+            </div>
+          </div>
+        </div>
+        {/* ----------------------------- */}
         <div className='container mx-auto'>
           <h2
             id='member'
