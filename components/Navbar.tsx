e9c52ae16d6be73750cdea31083fc6a7bd8bc62d
hro19 <fourbasic19@gmail.com>
2024-02-11 20:36:54 +0900

    【framermotion】type: 'spring'

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -65,12 +65,20 @@ const Navbar = () => {
                 </li>
                 <li>
                   <Link
+                    href='/framer'
+                    className='block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500'
+                  >
+                    Framer motionラボ
+                  </Link>
+                </li>
+                {/* <li>
+                  <Link
                     href='/gallery'
                     className='block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500'
                   >
                     画像ライブラリ
                   </Link>
-                </li>
+                </li> */}
                 <li>
                   <Link
                     href='/graph'
--- pages/framer.tsx
+++ pages/framer.tsx
@@ -5,6 +5,18 @@ const framer = () => {
   return (
     <>
       <h1 className='font-bold from-neutral-900 text-2xl underline'>
+        springお試し
+      </h1>
+      <motion.button
+        initial={{ x: '-50px', opacity: '0' }}
+        animate={{ x: 0, opacity: '1' }}
+        transition={{ type: 'spring', stiffness: 140 }}
+        className='text-xl text-zinc-50 bg-blue-500 p-4 ml-2'
+      >
+        springお試し
+      </motion.button>
+
+      <h1 className='font-bold from-neutral-900 text-2xl underline'>
         オンhover
       </h1>
       <motion.button
