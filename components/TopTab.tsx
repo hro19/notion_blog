eea00d84d1d3abe198cc22826d1a2fda79994dd3
hro19 <fourbasic19@gmail.com>
2024-02-16 05:28:07 +0900

    【yamadaUI】muiをyamadaUIに書き換える

--- components/TopTab.tsx
+++ components/TopTab.tsx
@@ -1,71 +1,62 @@
 import React, { useState } from 'react';
 import Link from 'next/link';
-import { Box, Tab, Tabs, Button } from '@mui/material';
+import { Box, Button, ButtonGroup } from '@yamada-ui/react';
 
 type Props = {
   allTags: string[];
 };
 
 const TopTab = ({ allTags }: Props) => {
-  const [value, setValue] = useState(0);
-  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
-    setValue(newValue);
-  };
+  // const [value, setValue] = useState(0);
+  // const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+  //   setValue(newValue);
+  // };
 
   return (
     <div className='my-3'>
       <Box sx={{ width: '100%' }}>
-        <Tabs
+        {/* <Tabs
           value={value}
           onChange={handleChange}
           aria-label='basic tabs example'
         >
           <Tab label='カテゴリー' className='font-bold' />
           <Tab label='よく見るページ' className='font-bold' />
-        </Tabs>
-        {value === 0 && (
-          <Box>
-            <div className='text-2xl'>
-              {allTags.map((tag: string, index: number) => (
-                <Link
-                  href={`/tags/${tag}`}
-                  key={index}
-                  passHref
-                  className='mr-1'
+        </Tabs> */}
+        <Box>
+          <div className='text-2xl'>
+            {allTags.map((tag: string, index: number) => (
+              <Link href={`/tags/${tag}`} key={index} passHref className='mr-1'>
+                <Button
+                  key={tag}
+                  variant='outline'
+                  colorScheme='link'
+                  mx={'1px'}
                 >
-                  <Button
-                    key={tag}
-                    variant='outlined'
-                    color='primary'
-                    className='mx-1 text-lx'
-                  >
-                    {tag}
-                  </Button>
-                </Link>
-              ))}
-            </div>
-          </Box>
-        )}
-        {value === 1 && (
-          <Box>
-            <div>
-              <a
-                href='https://placehold.jp'
-                target='_blank'
-                className='text-blue-600 visited:text-purple-600 mx-2'
-              >
-                placeholder画像
-              </a>
-              <a
-                href='https://placehold.jp'
-                target='_blank'
-                className='text-blue-600 visited:text-purple-600 mx-2'
-              >
-                縦横中央揃え
-              </a>
-            </div>
-          </Box>
-        )}
+                  {tag}
+                </Button>
+              </Link>
+            ))}
+          </div>
+        </Box>
+        <Box>
+          <div>
+            <a
+              href='https://placehold.jp'
+              target='_blank'
+              className='text-blue-600 visited:text-purple-600 mx-2'
+            >
+              placeholder画像
+            </a>
+            <a
+              href='https://placehold.jp'
+              target='_blank'
+              className='text-blue-600 visited:text-purple-600 mx-2'
+            >
+              縦横中央揃え
+            </a>
+          </div>
+        </Box>
       </Box>
     </div>
   );
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,14 @@ import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import Layout from '../components/Layout';
 import '../public/image-gallery.css';
+import { UIProvider } from '@yamada-ui/react';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <UIProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </UIProvider>
   );
 }
