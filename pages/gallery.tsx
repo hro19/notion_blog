fdfd89668e2dff4df554576828deeff5b80a8470
hro19 <fourbasic19@gmail.com>
2024-02-16 05:26:39 +0900

    型を正しく修正

--- pages/gallery.tsx
+++ pages/gallery.tsx
@@ -1,10 +1,12 @@
 import { useState, useEffect } from 'react';
 // import Gallery from 'react-easy-image-gallery';
+import Image from 'next/image';
+import { AspectRatio } from '@yamada-ui/react';
 
 interface Image {
   src: string;
-  width: string;
-  height: string;
+  width: number;
+  height: number;
 }
 
 export default function Gallery() {
@@ -21,13 +23,16 @@ export default function Gallery() {
 
   return (
     <div>
-      {images.map((image) => (
-        <img
-          src={image.src}
-          width={image.width}
-          height={image.height}
-          key={image.src}
-        />
+      {images.map((image, index) => (
+        <AspectRatio w='md' ratio={4 / 3} key={index}>
+          <Image
+            key={index}
+            src={image.src}
+            width={image.width}
+            height={image.height}
+            alt='gallery image'
+          />
+        </AspectRatio>
       ))}
     </div>
   );
